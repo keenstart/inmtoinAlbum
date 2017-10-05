c9ae305df3713ba8e8d9aa72a7c80f4d02015bcf
Gareth harris <memhereroot@gmail.com>
2017-10-05 17:23:53 -0500

    Open Album and Display Photo in views

---
+++ Album/View/PhotoCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionCell.swift
+//  Album
+//
+//  Created by Gareth Harris on 10/5/17.
+//  Copyright © 2017 Gareth Harris. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionCell: UICollectionViewCell {
+    
+}
